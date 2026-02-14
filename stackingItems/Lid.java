dfde554c1c13e7647150e5516a05762c0a7667ab
Yeray Guachetá Carreño <yeye20041@gmail.com>
2026-02-14 11:05:50 -0500

    creating new classes

---
+++ stackingItems/Lid.java
@@ -0,0 +1,30 @@
+/**
+ * Write a description of class Lid here.
+ * 
+ * @author Andrés Sotelo
+ * @author Yeray Guachetá
+ * 
+ * @version 1.0
+ */
+public class Lid{
+
+    private static final int height = 1;
+
+    /**
+     * Constructor for objects of class Lid
+     */
+    public Lid(){
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y){
+        // put your code here
+        return y;
+    }
+}
